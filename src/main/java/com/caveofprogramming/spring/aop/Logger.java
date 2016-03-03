2893a05e8afc7691ec28147958d55e0aba56e0f3
Jacek135 <jac.poczatek@gmail.com>
2016-03-03 06:12:23 -0800

    79 done

--- src/main/java/com/caveofprogramming/spring/aop/Logger.java
+++ src/main/java/com/caveofprogramming/spring/aop/Logger.java
@@ -1,7 +1,13 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class Logger {
+    @After("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
     public void aboutToTakePhoto(){
         System.out.println("About to take a photo...");
     }
