7c9a34796fce49500bbc181f8ef67b24832a1467
Jacek135 <jac.poczatek@gmail.com>
2016-03-03 02:02:39 -0800

    Initial commit

---
+++ src/main/java/com/caveofprogramming/spring/aop/App.java
@@ -0,0 +1,15 @@
+package com.caveofprogramming.spring.aop;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App {
+    public static void main (String [] args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/aop/bean.xml");
+
+        Camera camera = (Camera)context.getBean("camera");
+        camera.snap();
+
+        context.close();
+    }
+}
